adeb00644dac39ed62df576652760490a130c4bd
Clemens Beißel <clemens@buildarocket.com>
2025-07-05 21:24:08 +0200

    add Reset Button

--- src/admin/ResetButton.jsx
+++ src/admin/ResetButton.jsx
@@ -6,7 +6,7 @@ const ResetButton = ({ socket }) => {
   }
   return (
     <button
-      className="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
+      className="text-red-700 hover:text-white border border-red-700 hover:bg-red-800  focus:outline-none  font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 "
       onClick={reset}
     >
       RESET
